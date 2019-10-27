8b42cd5cebc8d55b6e95ecc2332435d17de99e1f
Probeloff <degtyarev.vg@mail.ru>
2019-10-27 21:44:37 +0700

    UImini, Router, Tasks

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,50 @@
 <template lang="pug">
-  #app
-    h1.test Hellooooo!
+  .wrapper
+    header
+      .navbar
+        .container
+          .navbar-content
+            router-link.header-logo(
+              to="/"
+            ) Film library
+            .button-burger(
+              @click="menuShow = !menuShow"
+              :class="{ active: menuShow }"
+            )
+              span.line.line-1
+              span.line.line-2
+              span.line.line-3
+            .navbar-list__wrapper(
+              :class="{ active: menuShow }"
+            )
+              ul.navbar-list
+                li.navbar-item(
+                  v-for="link in linkMenu"
+                  :key="link.title"
+                  @click="menuShow = false"
+                )
+                  router-link.navbar-link(
+                    :to="`${link.url}`"
+                  ) {{ link.title }}
+
+    router-view
 </template>
 
 <script>
 export default {
-  name: 'App'
+  data () {
+    return {
+      menuShow: false,
+      linkMenu: [
+        {title: 'Home', url: '/'},
+        {title: 'Login', url: '/login'},
+        {title: 'Registration', url: '/registration'}
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="stylus">
-  .test
-    color: green
+  @import './assets/stylus/uimini.min.css';
 </style>
